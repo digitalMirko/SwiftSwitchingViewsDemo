53282591ead3d4289eab4b45c847ef40ad4e2e62
Mirko Cukich <mirkocukich@gmail.com>
2019-09-22 23:51:12 -0500

    Program Complete

---
+++ SwitchingViews Demo/ThirdViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdViewController.swift
+//  SwitchingViews Demo
+//
+//  Created by Mirko Cukich on 9/22/19.
+//  Copyright © 2019 Digital Mirko. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func mainViewBtn(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+}
